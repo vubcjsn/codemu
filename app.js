47f5130a608a45fa41f38ffd853bf7ba84d6982c
Azamatova Begimai <b.a.azamatova@gmail.com>
2025-02-09 18:20:01 +0600

    шаблонные строки и все остальные до 31 стр

---
+++ app.js
@@ -0,0 +1,42 @@
+"use strict";
+//alert("text!");
+
+//let num = 123;
+//alert(num);
+
+//let a, b, c;
+
+//a = 1;
+//b = 2;
+//c = 3;
+
+//let a = 1;
+//let b = 2;
+//let c = 3;
+
+//let a = 13;
+//let b = 5;
+//alert(a % b);
+
+//let a = 2;
+//let b = 3;
+//alert(a ** b);
+
+//let a = 3 * 2 ** 3;
+//alert(a);
+
+//let str = "!!!";
+//alert(str);
+
+//let str1 = "Java";
+//let str2 = "Script";
+//alert(str1 + str2);
+/*let str = "hello";
+alert(str + " " + "World");*/
+
+/*let str = "лебеди";
+alert(str.length);*/
+let str1 = "xxx";
+let str2 = "yyy";
+alert(xxx $ {str} yyy ${str} aaa
+);
