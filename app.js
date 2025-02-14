e7e4a84e6a2861e4b9be14e566fc020be6c7fec9
Azamatova Begimai <b.a.azamatova@gmail.com>
2025-02-14 15:03:05 +0600

    поиск ошибок в масивах

--- app.js
+++ app.js
@@ -336,3 +336,12 @@ delete arr[1];
 delete arr[3];
 console.log(arr);
 console.log(arr.length);*/
+//Код должен вывести последний элемент массива:
+/*let arr = [1, 2, 3, 4, 5];
+console.log(arr[arr.length - 1]);*/
+//Код должен найти сумму элементов массива:
+/*let arr = [1, 2, 3, 4, 5];
+console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);*/
+//Код должен вывести длину массива:
+/*let arr = [1, 2, 3, 4, 5];
+console.log(arr.length);*/
