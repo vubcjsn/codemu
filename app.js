7a59bdf1602b9218ca510b97f880c60f3cd18102
Azamatova Begimai <b.a.azamatova@gmail.com>
2025-02-16 19:00:08 +0600

    Оператор in

--- app.js
+++ app.js
@@ -443,3 +443,10 @@ let obj = {
   z: 3,
 };
 console.log(obj);*/
+
+//94
+
+//Скажите, что выведется в консоль в результате выполнения следующего кода:
+/*let obj = { x: 1, y: 2, z: 3 };
+console.log("x" in obj);
+console.log("w" in obj);*/
