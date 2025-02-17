6af4c4cd148f547599e7058271923e89faa14cb9
Azamatova Begimai <b.a.azamatova@gmail.com>
2025-02-17 14:33:09 +0600

    Сокращенная форма проверки на ложь

--- app.js
+++ app.js
@@ -638,3 +638,22 @@ if (true) {
 } else {
   console.log("---");
 }*/
+//Перепишите следующий код с использованием сокращенной формы:
+/*
+let test = true;
+
+if (!test) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+*/
+//Перепишите следующий код с использованием сокращенной формы:
+/*
+let test = true;
+
+if (!test) {
+  console.log("+++");
+} else {
+  console.log("---");
+}*/
