1e1b01261425b84d243923988d8a64c686dcc31d
Azamatova Begimai <b.a.azamatova@gmail.com>
2025-02-17 14:45:45 +0600

    Сложные условия в сокращенной форме

--- app.js
+++ app.js
@@ -676,3 +676,44 @@ if (test) {
   console.log("---");
 }
 */
+//Перепишите следующий код в сокращенной форме:
+/*
+let test1 = true;
+let test2 = true;
+
+if (test1 && test2) {
+  console.log("+++");
+} else {
+  console.log("---");
+}*/
+//Перепишите следующий код в сокращенной форме:
+/*
+let test1 = true;
+let test2 = true;
+
+if (test1 && test2) {
+  console.log("+++");
+} else {
+  console.log("---");
+}*/
+//Перепишите следующий код в сокращенной форме:
+/*
+let test1 = true;
+let test2 = true;
+
+if (test1 && test2) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+Перепишите следующий код в сокращенной форме:*/
+/*
+let test1 = true;
+let test2 = true;
+
+if (test1 && test2) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+*/
