80352452231a2a53db4f9a3b019c898857d53763
Azamatova Begimai <b.a.azamatova@gmail.com>
2025-02-17 15:37:17 +0600

    Нюансы области видимости переменных в конструкциях if-else

--- app.js
+++ app.js
@@ -811,9 +811,21 @@ let age = 17;
 let adult;
 
 if (age >= 18) {
-  let adult = true;
+  adult = true;
 } else {
-  let adult = false;
+  adult = false;
+}
+
+console.log(adult);*/
+//Автор приведенного ниже кода хотел выполнить проверку возраста на достижение 18 лет. Код, однако, при любом значение возраста выводит в консоль значение undefined. Исправьте ошибку автора кода. Вот проблемный код:
+/*
+let age = 17;
+let adult;
+
+if (age >= 18) {
+  adult = true;
+} else {
+  adult = false;
 }
 
 console.log(adult);*/
